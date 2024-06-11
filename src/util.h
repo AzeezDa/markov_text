84452175af624db9acc282b667eae1c7da3bd035
AzeezDa <azeez.daoud3@gmail.com>
2024-06-11 10:29:04 +0200

    Fix bug in I/O binary read/write that causes problems in gcc -O3 exectuables

--- src/util.h
+++ src/util.h
@@ -1,16 +1,25 @@
 #pragma once
 #include <iostream>
+#include <type_traits>
 
 template <typename T>
 constexpr size_t binary_write(std::ostream& out, const T& value) {
-    out.write(reinterpret_cast<const char*>(&value), sizeof(T));
-
-    return sizeof(T);
+    if constexpr (std::is_same<T, std::string>::value) {
+        out << value << '\0';
+        return value.size() + 1;
+    } else {
+        out.write(reinterpret_cast<const char*>(&value), sizeof(T));
+        return sizeof(T);
+    }
 }
 
 template <typename T>
 constexpr size_t binary_read(std::istream& in, T& value) {
-    in.read(reinterpret_cast<char*>(&value), sizeof(T));
-
-    return sizeof(T);
+    if constexpr (std::is_same<T, std::string>::value) {
+        std::getline(in, value, '\0');
+        return value.size() + 1;
+    } else {
+        in.read(reinterpret_cast<char*>(&value), sizeof(T));
+        return sizeof(T);
+    }
 }
