9c8b568f4cae9cb34caa2a3ca28785726db4dce1
AzeezDa <azeez.daoud3@gmail.com>
2024-06-15 21:07:14 +0200

    Make binary io C++20-friendly by replacing is_pod

--- src/binary_io.hpp
+++ src/binary_io.hpp
@@ -8,14 +8,17 @@
 template <typename T>
 struct dependable_false : std::false_type {};
 
+template <typename T>
+inline constexpr bool is_binary_streamable = std::is_trivial_v<T> && std::is_standard_layout_v<T>;
+
 // Write a value in binary to the output stream. Strings are written byte by byte,
 // and are null-terminated. Other values are written as they are.
 template <typename T>
 constexpr std::size_t binary_write(std::ostream& out, const T& value) {
-    if constexpr (std::is_same<T, std::string>::value) {
+    if constexpr (std::is_same_v<T, std::string>) {
         out << value << '\0';
         return value.size() + 1;
-    } else if constexpr (std::is_pod<T>::value) {
+    } else if constexpr (is_binary_streamable<T>) {
         out.write(reinterpret_cast<const char*>(&value), sizeof(T));
         return sizeof(T);
     } else {
@@ -28,10 +31,10 @@ constexpr std::size_t binary_write(std::ostream& out, const T& value) {
 // and must be null-terminated. Other values are read as they are.
 template <typename T>
 constexpr std::size_t binary_read(std::istream& in, T& value) {
-    if constexpr (std::is_same<T, std::string>::value) {
+    if constexpr (std::is_same_v<T, std::string>) {
         std::getline(in, value, '\0');
         return value.size() + 1;
-    } else if constexpr (std::is_pod<T>::value) {
+    } else if constexpr (is_binary_streamable<T>) {
         in.read(reinterpret_cast<char*>(&value), sizeof(T));
         return sizeof(T);
     } else {
