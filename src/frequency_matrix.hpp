8e12d69a22fc5ec76ce5f03da2faa579ba7bdbfb
AzeezDa <azeez.daoud3@gmail.com>
2024-06-15 10:16:42 +0200

    Make sequence hasher a functor rather than inherit from std::hash

--- src/frequency_matrix.hpp
+++ src/frequency_matrix.hpp
@@ -6,12 +6,9 @@
 #include <vector>
 
 using sequence = std::vector<std::size_t>;
-using frequency_row = std::unordered_map<std::size_t, std::size_t>;
-using frequency_matrix = std::unordered_map<sequence, frequency_row>;
 
 // Based on the answer by see in: https://stackoverflow.com/questions/20511347/a-good-hash-function-for-a-vector
-template <>
-struct std::hash<sequence> {
+struct sequence_hasher {
     std::size_t operator()(const sequence& sequence) const noexcept {
         std::size_t seed = sequence.size();
         for (auto n : sequence) {
@@ -23,6 +20,9 @@ struct std::hash<sequence> {
         return seed;
     }
 };
+
+using frequency_row = std::unordered_map<std::size_t, std::size_t>;
+using frequency_matrix = std::unordered_map<sequence, frequency_row, sequence_hasher>;
 
 class ChainConstructor;
 
